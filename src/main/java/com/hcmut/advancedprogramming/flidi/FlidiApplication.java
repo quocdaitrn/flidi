4b064718b3061c3fd7297927001195079221ee2b
quocdaitrn <daitq.cntt@gmail.com>
2018-11-06 22:03:09 +0700

    init project

---
+++ src/main/java/com/hcmut/advancedprogramming/flidi/FlidiApplication.java
@@ -0,0 +1,12 @@
+package com.hcmut.advancedprogramming.flidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlidiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlidiApplication.class, args);
+    }
+}
