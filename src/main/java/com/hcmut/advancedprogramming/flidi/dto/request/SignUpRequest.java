0d44df6f2a9b1483246c641e228692fa40aec67b
quocdaitrn <daitq.cntt@gmail.com>
2018-11-08 09:19:15 +0700

    implement signup feature

---
+++ src/main/java/com/hcmut/advancedprogramming/flidi/dto/request/SignUpRequest.java
@@ -0,0 +1,89 @@
+package com.hcmut.advancedprogramming.flidi.dto.request;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+public class SignUpRequest {
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String firstName;
+
+    @NotBlank
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @NotBlank
+    @Size(min = 3, max = 50)
+    private String username;
+
+    @NotBlank
+    @Size(max = 100)
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+
+    @Size(max = 255)
+    private String avatar;
+
+    private Boolean active;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+}
--- src/main/java/com/hcmut/advancedprogramming/flidi/web/controller/AuthController.java
+++ src/main/java/com/hcmut/advancedprogramming/flidi/web/controller/AuthController.java
@@ -1,11 +1,18 @@
 package com.hcmut.advancedprogramming.flidi.web.controller;
 
 import com.hcmut.advancedprogramming.flidi.dto.request.LoginRequest;
+import com.hcmut.advancedprogramming.flidi.dto.request.SignUpRequest;
+import com.hcmut.advancedprogramming.flidi.dto.response.ApiResponse;
 import com.hcmut.advancedprogramming.flidi.dto.response.JwtAuthenticationResponse;
+import com.hcmut.advancedprogramming.flidi.exception.AppException;
+import com.hcmut.advancedprogramming.flidi.persistence.enumtype.RoleName;
+import com.hcmut.advancedprogramming.flidi.persistence.model.Role;
+import com.hcmut.advancedprogramming.flidi.persistence.model.User;
 import com.hcmut.advancedprogramming.flidi.persistence.repository.RoleRepository;
 import com.hcmut.advancedprogramming.flidi.persistence.repository.UserRepository;
 import com.hcmut.advancedprogramming.flidi.security.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -16,8 +23,12 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import javax.validation.Valid;
+import java.net.URI;
+import java.util.Collections;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -51,5 +62,38 @@ public class AuthController {
 
         String jwt = tokenProvider.generateToken(authentication);
         return ResponseEntity.ok(new JwtAuthenticationResponse(jwt));
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<?> registerUser(@Valid @RequestBody SignUpRequest signUpRequest) {
+        if (userRepository.existsByUsername(signUpRequest.getUsername())) {
+            return new ResponseEntity(new ApiResponse(false, "Username is already taken!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        if (userRepository.existsByEmail(signUpRequest.getEmail())) {
+            return new ResponseEntity(new ApiResponse(false, "Email Address already in use!"),
+                    HttpStatus.BAD_REQUEST);
+        }
+
+        // Creating user's account
+        User user = new User(null, signUpRequest.getUsername(), signUpRequest.getPassword(), signUpRequest.getEmail(),
+                signUpRequest.getFirstName(), signUpRequest.getLastName(), signUpRequest.getAvatar(),
+                signUpRequest.getActive(), new Date());
+
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        Role userRole = roleRepository.findByName(RoleName.ROLE_USER)
+                .orElseThrow(() -> new AppException("User Role not set."));
+
+        user.setRoles(Collections.singleton(userRole));
+
+        User result = userRepository.save(user);
+
+        URI location = ServletUriComponentsBuilder
+                .fromCurrentContextPath().path("/api/users/{username}")
+                .buildAndExpand(result.getUsername()).toUri();
+
+        return ResponseEntity.created(location).body(new ApiResponse(true, "User registered successfully"));
     }
 }
