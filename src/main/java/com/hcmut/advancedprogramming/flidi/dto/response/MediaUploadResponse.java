776732ce47c7c435c5a5c7574b4c51dfc1b27898
quocdaitrn <daitq.cntt@gmail.com>
2018-12-08 11:32:26 +0700

    using lombok for response payload

--- src/main/java/com/hcmut/advancedprogramming/flidi/dto/response/MediaUploadResponse.java
+++ src/main/java/com/hcmut/advancedprogramming/flidi/dto/response/MediaUploadResponse.java
@@ -1,47 +1,15 @@
 package com.hcmut.advancedprogramming.flidi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class MediaUploadResponse {
     private String fileName;
     private String fileDownloadUri;
     private String fileType;
     private long size;
-
-    public MediaUploadResponse(String fileName, String fileDownloadUri, String fileType, long size) {
-        this.fileName = fileName;
-        this.fileDownloadUri = fileDownloadUri;
-        this.fileType = fileType;
-        this.size = size;
-    }
-
-    public String getFileName() {
-        return fileName;
-    }
-
-    public void setFileName(String fileName) {
-        this.fileName = fileName;
-    }
-
-    public String getFileDownloadUri() {
-        return fileDownloadUri;
-    }
-
-    public void setFileDownloadUri(String fileDownloadUri) {
-        this.fileDownloadUri = fileDownloadUri;
-    }
-
-    public String getFileType() {
-        return fileType;
-    }
-
-    public void setFileType(String fileType) {
-        this.fileType = fileType;
-    }
-
-    public long getSize() {
-        return size;
-    }
-
-    public void setSize(long size) {
-        this.size = size;
-    }
 }
