72ba67ca3ea0089f4e837e31fd27cdc4d813b018
quocdaitrn <daitq.cntt@gmail.com>
2018-12-08 11:05:22 +0700

    using lombok

--- src/main/java/com/hcmut/advancedprogramming/flidi/dto/response/UserSummary.java
+++ src/main/java/com/hcmut/advancedprogramming/flidi/dto/response/UserSummary.java
@@ -1,38 +1,15 @@
 package com.hcmut.advancedprogramming.flidi.dto.response;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
 public class UserSummary {
     private Long id;
     private String username;
     private String name;
-
-    public UserSummary(Long id, String username, String name) {
-        this.id = id;
-        this.username = username;
-        this.name = name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
